8111f165c56d9d7311cff2999b3a00985997cdc5
Diego Ardila <63212406+diego1286@users.noreply.github.com>
2022-08-11 22:11:53 -0500

    :memo:  Creacion del repo para el proyecto de MinTic

---
+++ src/main/java/com/co/mintic/ciclo3mintic/Ciclo3MinTicApplication.java
@@ -0,0 +1,13 @@
+package com.co.mintic.ciclo3mintic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ciclo3MinTicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ciclo3MinTicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/co/mintic/ciclo3mintic/Ciclo3MinTicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.mintic.ciclo3mintic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ciclo3MinTicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
